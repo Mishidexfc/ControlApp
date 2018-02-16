bf61c4dc87d91481a7d582f6722b87f1fe26da1c
Mishidexfc <juewang@JuedeMacBook-Air.local>
2018-02-16 13:25:56 -0500

    Add Comments
    Add comments

--- ControlApp/LoginPage/LoginViewController.swift
+++ ControlApp/LoginPage/LoginViewController.swift
@@ -11,6 +11,7 @@
 //  Jan.23th.2018 Access Ewon login api and implement loading animation.
 //  Jan.24th.2018 Implement keychain store and biological id verification
 //  All api access functions are transferred to EwonApiCase class now.
+//  Feb.16th.2018 Add code comments and improve code logic.
 ////////////////////////////////////////////////////////////
 
 import UIKit
@@ -24,42 +25,42 @@ class LoginViewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        myViewContentInit()
         // Do any additional setup after loading the view, typically from a nib.
+        myViewContentInit()
         self.TF_password.isSecureTextEntry = true
         Act_Login_Loading.isHidden = true
         verifyBioPassword()
         
     }
+    
+    /// Drawing the layout and content for the view.
     private func myViewContentInit(){
         setTextFieldStyle(field: TF_account, imageName: "tag")
         setTextFieldStyle(field: TF_username, imageName: "tag")
         setTextFieldStyle(field: TF_password, imageName: "lock")
     }
+    
+    /// Set the text and its' style for single uitextfield.
     private func setTextFieldStyle(field:UITextField, imageName: String) {
         field.layer.borderWidth = 1
         field.layer.cornerRadius = 5
         field.layer.borderColor = UIColor.white.cgColor
-        /*if UIImage(named: imageName) != nil {
-            field.leftView = UIImageView(image: UIImage(named: imageName))
-            field.leftView?.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
-            field.leftViewMode = .always
-        }*/
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    /// Action for tapping help button with three options
+    /// Action for tapping help button to show alert with options
     @IBAction func HelpButton_TouchUpInside(_ sender: UIButton) {
         let popbottom = UIAlertController(title: "Help", message: "Contact us if need account & password", preferredStyle: .actionSheet)
-        /// Option: Contact Alfa Laval, dial the number of niagara blowers
+        /// Option: Contact Company, dial the number.
         popbottom.addAction(UIAlertAction(title: NSLocalizedString("Contact Alfa Laval", comment: "Default action"), style: .destructive, handler: { _ in
             guard let number = URL(string: "tel://7168752000") else { return }
             UIApplication.shared.open(number)
         }))
-        /// Option: Term of use, currently visit the 'about us' web page in al niagara website
+        /// Option: Term of use, currently visit the 'about us' web page in company's website
         popbottom.addAction(UIAlertAction(title: NSLocalizedString("Term of use", comment: "Default action"), style: .default, handler: { _ in
             guard let webAddress = URL(string: "http://www.niagarablower.com/About") else { return }
             UIApplication.shared.open(webAddress, options: [:], completionHandler: nil)
@@ -77,6 +78,7 @@ class LoginViewController: UIViewController{
         /// Verification here
         Act_Login_Loading.isHidden = false
         Act_Login_Loading.startAnimating()
+        /// Send credentials to Ewon by using login api.
         ewon.verifyKeychain(account: TF_account.text!, userName: TF_username.text!, password: TF_password.text!, completion: self.permitLogin(result:code:))
         ///
     }
@@ -86,11 +88,13 @@ class LoginViewController: UIViewController{
         self.view.endEditing(false)
     }
     
+    /// When login api responses, handle different results.
     private func permitLogin(result:String,code:Int) {
         Act_Login_Loading.stopAnimating()
         self.Act_Login_Loading.isHidden = true
         switch result {
         case "fail":
+            /// Credentials wrong or exceed the connection limits(simple user limits 3 connections.)
             let tempWarning : String = "Your acccount/username/password combination is invalid or too many concurrent connections.\nPlease try again later."
             denyLogin(warning: tempWarning)
             break
@@ -99,21 +103,22 @@ class LoginViewController: UIViewController{
             denyLogin(warning: tempWarning)
             break
         default:
+            /// using default because api returns session number when login successfully.
             storeKeyChain(account: TF_account.text!, userName: TF_username.text!, password: TF_password.text!, session: result)
-            /** Head to main page.
-             The id verification should be done before this segue.
-             */
+            /// Head to main page, the id verification should be done before this segue.
             self.performSegue(withIdentifier: "LoginToStatus", sender: self)
             break
         }
     }
     
+    /// deny the login action when failing to login.
     private func denyLogin(warning:String) {
         let alert = UIAlertController(title: "Login Failed", message: warning, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         self.present(alert, animated: true, completion: nil)
     }
     
+    /// Store credentials and other private information in memory.
     private func storeKeyChain(account:String, userName:String, password: String, session: String) {
         userParameter.t2maccount = account
         userParameter.t2musername = userName
@@ -125,6 +130,7 @@ class LoginViewController: UIViewController{
         }
     }
     
+    /// If stored information exists, devices which support touch id or face id can verify identity easily.
     private func verifyBioPassword(){
         let context = LAContext()
         var error: NSError?
